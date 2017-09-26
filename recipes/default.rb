c54644a4fe20b1595cb58602121ae1dc99a75305
Andrey Sidorov <takandar@gmail.com>
2017-09-26 14:05:05 +0300

    do not create dirs here

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,11 +9,6 @@ include_recipe 'memcached::default'
 include_recipe 'appbox::users'
 include_recipe 'appbox::apps_dir'
 
-node['app']['dirs'].each do |key, dir|
-  next if key == 'root'
-  mkdir dir
-end
-
 include_recipe 'chef_rails_base::packages'
 
 hostname node['app']['hostname'] if node['app']['hostname']
