af12ea6492ea63a9246823996224f81b980bc977
Andrey Sidorov <takandar@gmail.com>
2018-05-27 19:18:17 +0300

    remove deprecation warning

--- resources/mkdir.rb
+++ resources/mkdir.rb
@@ -6,7 +6,7 @@ property :owner, default: node['app']['user']['name']
 action :create do
   # don't set recursive to true - owner, group and mode
   # options are not applied to created parent directories
-  directory path do
+  directory new_resource.path do
     owner new_resource.owner
     group 'apps'
     mode '0775'
